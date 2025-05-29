786efbbf1902f29a08b698e9641b7e66e23d1200
chrollos <106138109+chro11os@users.noreply.github.com>
2025-05-29 17:30:28 +0800

    created a .envlocal for vercel deployment

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
